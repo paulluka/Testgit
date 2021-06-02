a19dfc7d4c7ddfeca9971cf669376d634bfe82c5
paulluka <paul.luka@hotmail.fr>
2021-06-02 16:12:26 +0200

    Premiere sauvegarde

---
+++ index.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
+st.color_picker('Pick a color')
